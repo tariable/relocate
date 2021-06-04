7dafd5d0f8fff6ed5dcaa9efe0e25e3bcaf7c9be
Maksix <MLeSurzhikov@sberbank.ru>
2021-06-04 21:05:48 +0300

    init project

---
+++ front/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import test from './App.module.scss';
+
+const App = () => {
+  return (
+    <div className={test.red}>
+      here
+    </div>
+  );
+}
+
+export default App;

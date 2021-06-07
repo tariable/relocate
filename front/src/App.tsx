a72c6a88fbf8479ae7a5c39beef1be82cf099c0b
Maksix <MLeSurzhikov@sberbank.ru>
2021-06-07 09:11:32 +0300

    header

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.less';
-import { Button } from 'antd';
+import Header from './Header';
 
-const App = () => (
+const App: React.FC = () => (
   <div>
-    <Button type="primary">Button</Button>
+    <Header />
   </div>
 );
 

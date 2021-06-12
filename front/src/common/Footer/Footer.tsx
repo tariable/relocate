4dbe2e85687bbf7b5a9d1e9e552aa1a46b827d98
Maksix <MLeSurzhikov@sberbank.ru>
2021-06-12 22:40:14 +0300

    added footer

--- front/src/common/Footer/Footer.tsx
+++ front/src/common/Footer/Footer.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import './Footer.less';
+import { Link } from 'react-router-dom';
 
 const Footer: React.FC = () => (
-  <div>
-    footer
-  </div>
+  <footer className="footer">
+    <div className="row">
+      <div className="cell">Relocate</div>
+      <div className="cell">Арендаторам</div>
+      <div className="cell">Владельцам</div>
+    </div>
+    <div className="row">
+      <div className="cell">
+        <Link to="/about">
+          О нас
+        </Link>
+      </div>
+      <div className="cell">
+        <Link to="/about">
+          Найти квартиру
+        </Link>
+      </div>
+      <div className="cell">
+        <Link to="/about">
+          Сдать квартиру
+        </Link>
+      </div>
+    </div>
+    <div className="description">
+      Данный сайт является учебным проектом.
+      Данные, размещенные на нем, не соответствуют действительности и сгенерированы автоматически
+    </div>
+  </footer>
 );
 
 export default Footer;
--- front/src/common/Header/Header.tsx
+++ front/src/common/Header/Header.tsx
@@ -5,22 +5,26 @@ import { ReactComponent as Logo } from '../../icons/logo.svg';
 import './Header.less';
 
 const Header: React.FC = () => (
-  <Menu mode="horizontal">
-    <Menu.Item key="logo" icon={<Link to="/"><Logo /></Link>} />
-    <Menu.Item key="rent">
-      <Link to="/accommodation">
-        Снять
-      </Link>
-    </Menu.Item>
-    <Menu.Item key="login">
-      Войти
-    </Menu.Item>
-    <Menu.Item key="place">
-      <Link to="/">
-        Разместить объявление
-      </Link>
-    </Menu.Item>
-  </Menu>
+  <header>
+    <nav>
+      <Menu mode="horizontal">
+        <Menu.Item key="logo" icon={<Link to="/"><Logo /></Link>} />
+        <Menu.Item key="rent">
+          <Link to="/accommodation">
+            Снять
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="login">
+          Войти
+        </Menu.Item>
+        <Menu.Item key="place">
+          <Link to="/">
+            Разместить объявление
+          </Link>
+        </Menu.Item>
+      </Menu>
+    </nav>
+  </header>
 );
 
 export default Header;
